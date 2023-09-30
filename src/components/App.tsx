eb2e8f083c1b7ca08cd18105a71fef1040f707fe
Cordtus <96965330+Cordtus@users.noreply.github.com>
2023-09-30 15:38:45 -0600

    Update App.tsx

--- src/components/App.tsx
+++ src/components/App.tsx
@@ -1,97 +1,112 @@
-import React, { useState, useEffect } from "react";
-import { SigningStargateClient } from "@cosmjs/stargate";
-import { useWallet, ChainProvider } from "@cosmos-kit/react";
-import type { AppProps } from 'next/app';
-
-// Cosmos configuration
-const COSMOS_CONFIG = {
-  chain_id: "cosmoshub-4",
-  chainName: "Cosmos Hub 4",
-  rpc: "https://rpc.cosmos.directory",
-  rest: "https://lcd.cosmos.directory",
-  bech32Prefix: {
-    AccAddr: "cosmos",
-    AccPub: "cosmospub",
-    ValAddr: "cosmosvaloper",
-    ValPub: "cosmosvaloperpub",
-    ConsAddr: "cosmosvalcons",
-    ConsPub: "cosmosvalconspub",
-  },
-  coinType: 118,
-};
-
-export function LSMControl({ Component, pageProps }: AppProps) {
-  return (
-    <ChainProvider /* Insert your ChainProvider properties here */>
-      <Component {...pageProps} />
-    </ChainProvider>
-  );
-}
-
-function LSMToggle({ Component, pageProps }: AppProps) {
-  const wallet: any = useWallet();
-  const [result, setResult] = useState<string>("");
-
-  useEffect(() => {
-    connectWallet();
-  }, []);
-
-  const connectWallet = async () => {
-    try {
-      if (typeof window !== "undefined" && window.keplr) {
-        await wallet.activateWalletProvider("keplr");
-        await window.keplr.experimentalSuggestChain({
-          chain_id: COSMOS_CONFIG.chain_id,
-          chainName: COSMOS_CONFIG.chainName,
-          rpc: COSMOS_CONFIG.rpc,
-          rest: COSMOS_CONFIG.rest,
-          bip44: {
-            coinType: COSMOS_CONFIG.coinType,
-          },
-          bech32Config: COSMOS_CONFIG.bech32Prefix,
-          features: ["stargate", "ibc-transfer"],
+export const keplrStandardSignAndBroadcast = (
+  { address, keplr, msgs, chainId, memo = "" }: KeplrSignData,
+  statusCallback: (statusName: I18nKey) => void = () => undefined
+): BroadcastResponse =>
+  pipe(
+    TE.of({ keplr, chainId }),
+    TETap(() => statusCallback("signAndBroadcastStatus.keplrEnable")),
+    TE.chainW(enableKeplr),
+    TETap(() => statusCallback("signAndBroadcastStatus.getSigner")),
+    TE.chainW(() => getKeplrSigner({ keplr, chainId })),
+    TETap(() => statusCallback("signAndBroadcastStatus.connectingToChain")),
+    TE.chainW((signer) =>
+      pipe(
+        new Registry(defaultRegistryTypes),
+        TE.of,
+        TE.map((registry) => {
+          registry.register(
+            "/cosmos.staking.v1beta1.MsgCancelUnbondingDelegation",
+            MsgCancelUnbondingDelegation
+          );
+          registry.register(
+            "/cosmwasm.wasm.v1.MsgExecuteContract",
+            MsgExecuteContract
+          );
+          return registry;
+        }),
+        TE.chain((registry) =>
+          pipe(
+            fetchCSDKVersion(chainId),
+            TE.mapLeft(
+              createI18nError("authentication.csdkVersionFetchFailed")
+            ),
+            TE.chain((csdkVersion) =>
+              TE.tryCatch(
+                () =>
+                  SigningStargateClient.connectWithSigner(
+                    chainSettings[chainId].rpc,
+                    signer,
+                    {
+                      registry,
+                      // @ts-ignore tolerate the authz polyfill types
+                      aminoTypes:
+                        chainId !== "secret-4" &&
+                        (csdkVersion.includes("0.44") ||
+                          csdkVersion.includes("0.45"))
+                          ? authzPolyfillAminoTypes
+                          : customAminoTypes,
+                      gasPrice: GasPrice.fromString(
+                        chainSettings[chainId].gasPrice
+                      ),
+                      // accountParser,
+                    }
+                  ),
+                createI18nError("authentication.stargateClientUnavailable")
+              )
+            )
+          )
+        )
+      )
+    ),
+    TETap(() => statusCallback("signAndBroadcastStatus.simulating")),
+    TE.chainW((client) =>
+      TE.tryCatch(() => {
+        console.log("simulating", { address, msgs, memo });
+        return client
+          .simulate(address, msgs, memo)
+          .then((gas) => ({ gas, client }));
+      }, createI18nError("authentication.simulationError"))
+    ),
+    TETap(() =>
+      statusCallback("signAndBroadcastStatus.signingAndBroadcasting")
+    ),
+    TE.chainW(({ gas, client }) =>
+      TE.tryCatch(() => {
+        console.log("singing and broadcasting", {
+          client,
+          address,
+          msgs,
+          memo,
         });
-        await wallet.switchChain(COSMOS_CONFIG.chain_id);
-      } else {
-        setResult("Please install Keplr wallet extension");
-      }
-    } catch (error: unknown) {
-      console.error("Error connecting to Keplr", error);
-      setResult((error as Error).message);
-    }
-  };
-
-  const executeFunction = async (msgType: string) => {
-    try {
-      const signer = window.getOfflineSigner(COSMOS_CONFIG.chain_id);
-      const client = await SigningStargateClient.connectWithSigner(COSMOS_CONFIG.rpc, signer);
-  
-      const msg = {
-        typeUrl: msgType,
-        value: {
-          delegatorAddress: wallet.address,
-        },
-      };
-  
-      const response = await client.signAndBroadcast(wallet.wallet?.address || "", [msg], {
-        amount: [{ denom: "uatom", amount: "2000" }],
-        gas: "200000",
-      });
-      setResult(JSON.stringify(response, null, 2));
-    } catch (error: unknown) {
-      setResult((error as Error).message);
-    }
-  };
-
-  return (
-    <div>
-      <button onClick={() => executeFunction("/cosmos.staking.v1beta1.MsgDisableTokenizeShares")}>
-        Disable LSM
-      </button>
-      <button onClick={() => executeFunction("/cosmos.staking.v1beta1.MsgEnableTokenizeShares")}>
-        Enable LSM
-      </button>
-      <div>{result}</div>
-    </div>
+        return client.signAndBroadcast(
+          address,
+          msgs,
+          {
+            gas: Math.ceil(gas * 2).toString(),
+            amount: [
+              {
+                amount: (
+                  gas *
+                  2 *
+                  Number(
+                    flatTokenStringToCoin(chainSettings[chainId].gasPrice)
+                      ?.amount
+                  )
+                ).toString(),
+                denom: chainSettings[chainId].baseDenom,
+              },
+            ],
+          },
+          memo
+        );
+      }, createI18nError("authentication.failedToBroadcastKeplr"))
+    ),
+    TE.filterOrElseW(
+      (txResp) => txResp.code === 0,
+      (tx) => ["authentication.broadcastFailed", new Error(tx.rawLog)] as const
+    ),
+    TE.map((tx) => {
+      console.log("tx", tx);
+      return { txHash: tx.transactionHash };
+    })
   );
-}
