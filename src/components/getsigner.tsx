33946c8085e71e3d502093e59989712b1040620c
cordtus <cordtus@gmail.com>
2023-09-30 16:41:40 -0600

    Changes to be committed: 	modified:   src/components/App.tsx 	modified:   src/components/getsigner.tsx 	modified:   src/components/keplrwallet.tsx

--- src/components/getsigner.tsx
+++ src/components/getsigner.tsx
@@ -1,16 +1,11 @@
-import { SigningStargateClient } from "@cosmjs/proto-signing";
-import { createI18nError } from "@cosmjs/launchpad";
-// Define a function called getKeplrSigner that takes an object with two properties as its argument
 export const getKeplrSigner = ({
-  keplr, // A Keplr object
-  chainId, // An AvailableChain object
+  keplr,
+  chainId,
 }: {
   keplr: Keplr;
   chainId: AvailableChain;
 }) =>
-  // Use the fp-ts library to create a TaskEither object that attempts to get an offline signer from Keplr
   TE.tryCatch(
     () => keplr.getOfflineSignerAuto(chainId),
-    // If the above function fails, create an i18n error with the message "authentication.keplrSignerUnavailable"
     createI18nError("authentication.keplrSignerUnavailable")
   );
